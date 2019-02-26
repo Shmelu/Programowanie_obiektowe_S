ffea2270d3b34b0cf863a3b3ac428233270b1928
Anna Światłowska <swiatowskaania@gmail.com>
2019-02-26 14:43:36 +0100

    zadanie nieskonczone

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+   public class Car
+    {
+        private string _brand;
+        private string _model;
+        private int _doorCount;
+        private int _engineVolume;
+       private  _avgConsump_
+
+
+
+
+    }
+}
