7f8b83da8ce6234b14f9858e9f54196501417350
Mateusz_S <peshek009@asd.pl>
2019-02-26 14:42:48 +0100

    uytu

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+     public class Car
+    {
+        private string model;
+        private string broad;
+        private int numberOfDors;
+        private static int _carCount = 0;
+        
+        static void Main(string[] args)
+        {
+        }
+    }
+}
