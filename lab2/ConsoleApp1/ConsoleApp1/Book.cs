dcb58b21657b65e2f4766e9ebc9fc3b49e822db7
Starobzhanskyi <wstarobzanski@gmail.com>
2019-03-12 14:54:42 +0100

    add basic files

---
+++ lab2/ConsoleApp1/ConsoleApp1/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Book: Position
+    {
+        public int NumberOfPages { get; private set; }
+
+        public Book()
+        {
+            NumberOfPages = 0;
+        }
+
+        
+    }
+}
---
+++ lab2/ConsoleApp1/ConsoleApp1/Position.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class Position
+    {
+        public string Title { get; protected set; }
+        public int Id { get; protected set; }
+        public string PubHouse { get; protected set; }
+        public int YearOfPublish { get; protected set; }
+
+        public Position()
+        {
+            Title = null;
+            Id = 0;
+            PubHouse = null;
+            YearOfPublish = 0;
+        }
+
+        public Position(string _title,int _id, string _pubHouse, int _yearOfPublish)
+        {
+            Title = _title;
+            Id = _id;
+            PubHouse = _pubHouse;
+            YearOfPublish = _yearOfPublish;
+        }
+
+        public string toString()
+        {
+            return "title: " + title + ", id: " + Id + ", Publish House: " + PubHouse + ", Year of House: " + YearOfPublish;  
+        }
+    }
+}
