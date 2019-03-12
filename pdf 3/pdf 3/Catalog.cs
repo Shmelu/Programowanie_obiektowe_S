fe81087e3814527bdbe00b3d4e9f453d5211b374
marcin_szelag <marcin.szelg@gmail.com>
2019-03-12 14:51:03 +0100

    lab 3

---
+++ pdf 3/pdf 3/Catalog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pdf_3
+{
+    class Catalog
+    {
+        private string thematicDep;
+        public List<Position> posList { get; set; }
+        public Catalog()
+        {
+
+        }
+        public Catalog(string thematicDep_)
+        {
+
+        }
+        public void AddPosition(Position position_)
+        {
+            if(posList!=null)
+            posList.Add(position_);
+        }
+    }
+}
---
+++ pdf 3/pdf 3/Position.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pdf_3
+{
+    abstract class Position
+    {
+        protected string title;
+        protected int id;
+        protected string publisher;
+        protected int publishedIn;
+
+        public Position()
+        {
+            title = "brak";
+            id = 0;
+            publisher = "brak";
+            publishedIn = 0;
+        }
+        public Position(string title_, int id_,
+            string publisher_, int publishedIn_)
+        {
+            title = title_;
+            id = id_;
+            publisher = publisher_;
+            publishedIn = publishedIn_;
+        }
+        public void showInfo()
+        {
+            Console.WriteLine("Title: "+title);
+            Console.WriteLine("Id: "+id);
+            Console.WriteLine("Publisher: "+publisher);
+            Console.WriteLine("Year of publishment: "+publishedIn);
+        }
+    }
+}
